0b88df8333ab65a53dbf6d1320943d0284e738ae
Juniko <official.juniko@gmail.com>
2022-07-01 17:57:02 +0700

    fix validation joi

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -1,8 +1,17 @@
 import withJoi from "next-joi";
+import { response } from '@/lib/wrapper'
 
 export default withJoi({
   onValidationError: (_, res, error) => {
 
-    res.status(400).send(error);
+    if(error){
+      const errData = error.details.map((err)=>{
+        const data={};
+        data[err.context.key]=err.message;
+        return data;
+      });
+      console.log(errData)
+      return response(res, 'failed', {data: errData}, 'bad request', 400);
+    }
   },
 });
