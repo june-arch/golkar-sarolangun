025f65311c85792bef6ef2f3e7b87879ec83b235
M Juniko Dwi Putra <juniko@Ms-MacBook-Pro.local>
2022-07-08 20:08:49 +0700

    fix registrasi kta done

--- src/pages/api/v1/user/index.ts
+++ src/pages/api/v1/user/index.ts
@@ -11,7 +11,7 @@ import { uploadDiffMiddleware } from '@/controller/middleware/uploads'
 import validate from "@/controller/middleware/validation";
 import { configNext } from '@/controller/middleware/configNext'
 import { member } from '@/controller/dto/member.dto'
-import Tesseract from 'tesseract.js';
+import Tesseract, { createWorker } from 'tesseract.js';
 import { readFileSync } from 'fs'
 
 const handler = nextConnect<NextApiRequestModify, NextApiResponse>(configNext)
@@ -27,24 +27,33 @@ handler
       psm: 3
     }
     const img = readFileSync(files['photo_ktp'][0].destination + '/' + files['photo_ktp'][0].filename);
-    const result = await Tesseract.recognize(img,"eng",{ logger: m => console.log(m)});
-    console.log(result.data.text);
-    // const doc: Member = {
-    //   region_id,
-    //   nik,
-    //   fullname,
-    //   address,
-    //   phone_number,
-    //   email,
-    //   place_of_birth,
-    //   date_of_birth: new Date(date_of_birth),
-    //   gender,
-    //   status,
-    //   photo: file ? file.filename : '',
-    //   created_date: new Date(),
-    //   photo_ktp: file ? file.filename : '',
-    // };
-    // const result = await create(doc)
+    const worker = createWorker();
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    const ocr = await worker.recognize(img);
+    await worker.terminate();
+    const regex = new RegExp(`${nik}$`,'m');
+    if (!(ocr.data && regex.test(ocr.data.text))) {
+      response(res, 'failed', {data: null}, 'failed photo ktp tidak sesuai', 400);
+    }
+
+    const doc: Member = {
+      region_id: Number(region_id),
+      nik,
+      fullname,
+      address,
+      phone_number,
+      email,
+      place_of_birth,
+      date_of_birth: new Date(date_of_birth),
+      gender,
+      status: Number(status),
+      photo: files ? files['photo'][0].filename : '',
+      created_date: new Date(),
+      photo_ktp: files ? files['photo_ktp'][0].filename : '',
+    };
+    const result = await create(doc)
     response(res, 'success', {data: null}, 'success register member', 201);
   })
 
